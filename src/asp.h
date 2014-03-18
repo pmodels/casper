d813a9d80e45994bbe2a987ab18a931e2ec704fb
Pavan Balaji <balaji@mcs.anl.gov>
2014-03-17 23:16:51 -0500

    New code for ASP.

---
+++ src/asp.h
@@ -0,0 +1,16 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *  (C) 2014 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ *
+ */
+
+#if !defined ASP_H_INCLUDED
+#define ASP_H_INCLUDED
+
+#include "mpi.h"
+
+extern MPI_Comm asp_user_world;
+extern MPI_Comm asp_local_comm;
+
+#endif /* ASP_H_INCLUDED */
