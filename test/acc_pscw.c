5fabd2302a08ac5e3b61c8a7c8e68ec35b2bf4b7
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2014-09-04 15:47:28 -0500

    Added a test for PSCW.

---
+++ test/acc_pscw.c
@@ -0,0 +1,191 @@
+/*
+ * acclock.c
+ *  <FILE_DESC>
+ * 	
+ *  Author: Min Si
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <mpi.h>
+
+#define SLEEP_TIME 100  /* 100us */
+#define NUM_OPS 2
+#define TOTAL_NUM_OPS (NUM_OPS * 2)
+#define CHECK
+#define OUTPUT_FAIL_DETAIL
+
+double *winbuf = NULL;
+double *locbuf = NULL;
+int rank, nprocs;
+int comp_size = 1;
+MPI_Win win = MPI_WIN_NULL;
+int ITER = 2;
+double max_result = 0.0;
+double sum_result = 0.0;
+
+static int target_computation_init()
+{
+    return 0;
+}
+
+static int target_computation()
+{
+    double start = MPI_Wtime() * 1000 * 1000;
+    while (MPI_Wtime() * 1000 * 1000 - start < SLEEP_TIME);
+    return 0;
+}
+
+static int target_computation_exit()
+{
+    return 0;
+}
+
+static int run_test(int nop)
+{
+    int i, j, x, errs = 0, errs_total = 0;
+    MPI_Status stat;
+    int dst, org;
+    double sum = 0.0;
+    MPI_Group post_group = MPI_GROUP_NULL;
+    MPI_Group start_group = MPI_GROUP_NULL;
+    MPI_Group world_group = MPI_GROUP_NULL;
+    int source = 0;
+
+    MPI_Comm_group(MPI_COMM_WORLD, &world_group);
+
+    if (rank % 2) {
+        source = 1;
+        dst = (rank + 1) % nprocs;
+        MPI_Group_incl(world_group, 1, &dst, &start_group);
+
+        fprintf(stdout, "[%d]-----check start(%d)/ acc /complete(%d)\n", rank, dst, dst);
+    }
+    else {
+        source = 0;
+        org = (rank + nprocs - 1) % nprocs;
+        MPI_Group_incl(world_group, 1, &org, &post_group);
+
+        fprintf(stdout, "[%d]-----check post(%d)/wait(%d)\n", rank, org, org);
+    }
+
+    for (x = 0; x < ITER; x++) {
+        /* change date */
+        for (i = 0; i < NUM_OPS * nprocs; i++) {
+            locbuf[i] = x + i;
+        }
+
+        sum_result = 0.0;
+        for (i = 0; i < nop; i++) {
+            sum_result += locbuf[nop * rank + i];
+        }
+        max_result = locbuf[nop * rank + nop - 1];
+
+        if (source) {
+            MPI_Win_start(start_group, 0, win);
+
+            for (i = 0; i < nop; i++) {
+                MPI_Accumulate(&locbuf[i + dst * nop], 1, MPI_DOUBLE, dst, 0, 1,
+                               MPI_DOUBLE, MPI_SUM, win);
+                MPI_Accumulate(&locbuf[i + dst * nop], 1, MPI_DOUBLE, dst, 1, 1,
+                               MPI_DOUBLE, MPI_MAX, win);
+            }
+
+            MPI_Win_complete(win);
+            MPI_Barrier(MPI_COMM_WORLD);        /* will not start next epoch before target finish its check */
+        }
+        else {
+            MPI_Win_post(post_group, 0, win);
+            MPI_Win_wait(win);
+
+            /* source may start next start before this lock */
+
+            /* check result */
+            MPI_Win_lock(MPI_LOCK_EXCLUSIVE, rank, 0, win);
+            if (winbuf[0] != sum_result) {
+                fprintf(stderr, "[%d]winbuf[%d] %.1lf != %.1lf, iter %d\n", rank, 0,
+                        winbuf[0], sum_result, x);
+                errs++;
+            }
+            if (winbuf[1] != max_result) {
+                fprintf(stderr, "[%d]winbuf[%d] %.1lf != %.1lf, iter %d\n", rank, 1, winbuf[1],
+                        max_result, x);
+                errs++;
+            }
+
+            if (errs > 0) {
+                fprintf(stderr, "winbuf: %.1lf %.1lf\n", winbuf[0], winbuf[1]);
+            }
+
+            /* reset my window */
+            for (i = 0; i < 2; i++) {
+                winbuf[i] = 0.0;
+            }
+            MPI_Win_unlock(rank, win);
+
+            MPI_Barrier(MPI_COMM_WORLD);
+        }
+    }
+
+    MPI_Allreduce(&errs, &errs_total, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    if (post_group != MPI_GROUP_NULL)
+        MPI_Group_free(&post_group);
+    if (start_group != MPI_GROUP_NULL)
+        MPI_Group_free(&start_group);
+    if (world_group != MPI_GROUP_NULL)
+        MPI_Group_free(&world_group);
+
+    return errs_total;
+}
+
+int main(int argc, char *argv[])
+{
+    int size = NUM_OPS;
+    int i, errs = 0;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (nprocs < 2) {
+        fprintf(stderr, "Please run using at least 2 processes\n");
+        goto exit;
+    }
+
+    comp_size = SLEEP_TIME;
+    locbuf = calloc(NUM_OPS * nprocs, sizeof(double));
+
+    /* size in byte */
+    MPI_Win_allocate(sizeof(double) * 2, sizeof(double), MPI_INFO_NULL,
+                     MPI_COMM_WORLD, &winbuf, &win);
+
+    /*
+     * P0: SUM{0:NOPS-1}, MAX{0:NOPS-1}
+     * P1: SUM{NOPS:2NOPS-1}, MAX{NOPS:2NOPS-1}
+     * ...
+     */
+    for (i = 0; i < 2; i++) {
+        winbuf[i] = 0.0;
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    errs = run_test(size);
+
+    if (rank == 0) {
+        fprintf(stdout, "%d errors\n", errs);
+    }
+
+  exit:
+
+    if (win != MPI_WIN_NULL)
+        MPI_Win_free(&win);
+    if (locbuf)
+        free(locbuf);
+
+    MPI_Finalize();
+
+    return 0;
+}
