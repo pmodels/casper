143948d5c6fa8758d570dcbb8c50e38339bac697
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2014-09-18 13:10:33 -0500

    Added flush_local and flush_local_all.

---
+++ src/mpi/rma/win_flush_local_all.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mtcore.h"
+
+int MPI_Win_flush_local_all(MPI_Win win)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MTCORE_DBG_PRINT_FCNAME();
+
+    /* Simply translate to manticore flush */
+    mpi_errno = MPI_Win_flush_all(win);
+    if (mpi_errno != MPI_SUCCESS)
+        goto fn_fail;
+
+  fn_exit:
+    return mpi_errno;
+
+  fn_fail:
+    goto fn_exit;
+}
