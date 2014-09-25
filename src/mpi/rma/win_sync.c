ca110fbba8b7534aa9b206b45be674b5040d7fff
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2014-09-25 11:32:06 -0500

    Added win_sync.
    Win_sync is translated to sync on all internal windows of the current
    process, and sync on local shared window if it is locked for self target
    RMA.

---
+++ src/mpi/rma/win_sync.c
@@ -0,0 +1,41 @@
+/*
+ * win_sync.c
+ *  <FILE_DESC>
+ * 	
+ *  Author: Min Si
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "mtcore.h"
+
+int MPI_Win_sync(MPI_Win win)
+{
+    MTCORE_Win *uh_win;
+    int mpi_errno = MPI_SUCCESS;
+    int user_rank = 0;
+    int i;
+
+    MTCORE_DBG_PRINT_FCNAME();
+
+    MTCORE_Fetch_uh_win_from_cache(win, uh_win);
+    PMPI_Comm_rank(uh_win->user_comm, &user_rank);
+
+    if (uh_win->is_self_locked) {
+        mpi_errno = PMPI_Win_sync(uh_win->local_uh_win);
+        if (mpi_errno != MPI_SUCCESS)
+            goto fn_fail;
+    }
+
+    for (i = 0; i < uh_win->targets[user_rank].num_uh_wins; i++) {
+        mpi_errno = PMPI_Win_sync(uh_win->targets[user_rank].uh_wins[i]);
+        if (mpi_errno != MPI_SUCCESS)
+            goto fn_fail;
+    }
+
+  fn_exit:
+    return mpi_errno;
+
+  fn_fail:
+    return mpi_errno;
+}
