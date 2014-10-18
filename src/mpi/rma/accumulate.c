bc1ea6d9575d3672415cd6956cc4d7e04db11559
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2014-10-18 15:42:15 -0500

    Removed local RMA for accumulate operations.
    Cannot do local RMA because it breaks atomicity of accumulates.

--- src/mpi/rma/accumulate.c
+++ src/mpi/rma/accumulate.c
@@ -2,36 +2,6 @@
 #include <stdlib.h>
 #include "mtcore.h"
 
-static int MTCORE_Accumulate_shared_impl(const void *origin_addr,
-                                         int origin_count,
-                                         MPI_Datatype origin_datatype, int target_rank,
-                                         MPI_Aint target_disp,
-                                         int target_count, MPI_Datatype target_datatype,
-                                         MPI_Op op, MPI_Win win, MTCORE_Win * uh_win)
-{
-    int mpi_errno = MPI_SUCCESS;
-    MPI_Win *win_ptr = NULL;
-
-    MTCORE_Get_epoch_local_win(uh_win, win_ptr);
-
-    /* Issue operation to the target through local window, because shared
-     * communication is fully handled by local process.
-     */
-    mpi_errno = PMPI_Accumulate(origin_addr, origin_count, origin_datatype,
-                                uh_win->my_rank_in_uh_comm, target_disp, target_count,
-                                target_datatype, op, *win_ptr);
-    MTCORE_DBG_PRINT("MTCORE Accumulate to self(%d, in local win 0x%x)\n",
-                     uh_win->my_rank_in_uh_comm, *win_ptr);
-
-    goto fn_exit;
-
-  fn_exit:
-    return mpi_errno;
-
-  fn_fail:
-    goto fn_exit;
-}
-
 static int MTCORE_Accumulate_segment_impl(const void *origin_addr,
                                           int origin_count,
                                           MPI_Datatype origin_datatype, int target_rank,
@@ -109,75 +79,60 @@ static int MTCORE_Accumulate_impl(const void *origin_addr, int origin_count,
     int rank;
 
     PMPI_Comm_rank(uh_win->user_comm, &rank);
-#ifdef MTCORE_ENABLE_LOCAL_LOCK_OPT
-    if (target_rank == rank && uh_win->is_self_locked) {
-        /* If target is itself, we do not need translate it to any Helpers because
-         * win_lock(self) will force lock(helper) to be granted so that it is safe
-         * to send operations to the real target.
-         */
-        mpi_errno = MTCORE_Accumulate_shared_impl(origin_addr, origin_count,
-                                                  origin_datatype, target_rank, target_disp,
-                                                  target_count, target_datatype, op, win, uh_win);
+
+    /* Should not do local RMA in accumulate because of atomicity issue */
+
+    /* TODO: Do we need segment load balancing in fence ?
+     * 1. No lock issue.
+     * 2. overhead of data range checking and division */
+    if (MTCORE_ENV.lock_binding == MTCORE_LOCK_BINDING_SEGMENT &&
+        uh_win->targets[target_rank].num_segs > 1 && uh_win->epoch_stat == MTCORE_WIN_EPOCH_LOCK) {
+        mpi_errno = MTCORE_Accumulate_segment_impl(origin_addr, origin_count,
+                                                   origin_datatype, target_rank, target_disp,
+                                                   target_count, target_datatype, op, win, uh_win);
         if (mpi_errno != MPI_SUCCESS)
             return mpi_errno;
     }
-    else
-#endif
-    {
-        /* TODO: Do we need segment load balancing in fence ?
-         * 1. No lock issue.
-         * 2. overhead of data range checking and division */
-        if (MTCORE_ENV.lock_binding == MTCORE_LOCK_BINDING_SEGMENT &&
-            uh_win->targets[target_rank].num_segs > 1 &&
-            uh_win->epoch_stat == MTCORE_WIN_EPOCH_LOCK) {
-            mpi_errno = MTCORE_Accumulate_segment_impl(origin_addr, origin_count,
-                                                       origin_datatype, target_rank, target_disp,
-                                                       target_count, target_datatype, op, win,
-                                                       uh_win);
-            if (mpi_errno != MPI_SUCCESS)
-                return mpi_errno;
-        }
-        else {
-            /* Translation for intra/inter-node operations.
-             *
-             * We do not use force flush + shared window for optimizing operations to local targets.
-             * Because: 1) we lose lock optimization on force flush; 2) Although most implementation
-             * does shared-communication for operations on shared windows, MPI standard doesn’t
-             * require it. Some implementation may use network even for shared targets for
-             * shorter CPU occupancy.
-             */
-            int target_h_rank_in_uh = -1;
-            int data_size = 0;
-            MPI_Aint target_h_offset = 0;
-            MPI_Win *win_ptr = NULL;
+    else {
+        /* Translation for intra/inter-node operations.
+         *
+         * We do not use force flush + shared window for optimizing operations to local targets.
+         * Because: 1) we lose lock optimization on force flush; 2) Although most implementation
+         * does shared-communication for operations on shared windows, MPI standard doesn’t
+         * require it. Some implementation may use network even for shared targets for
+         * shorter CPU occupancy.
+         */
+        int target_h_rank_in_uh = -1;
+        int data_size = 0;
+        MPI_Aint target_h_offset = 0;
+        MPI_Win *win_ptr = NULL;
 
-            MTCORE_Get_epoch_win(target_rank, 0, uh_win, win_ptr);
+        MTCORE_Get_epoch_win(target_rank, 0, uh_win, win_ptr);
 
 #if defined(MTCORE_ENABLE_RUNTIME_LOAD_OPT)
-            if (MTCORE_ENV.load_opt == MTCORE_LOAD_BYTE_COUNTING) {
-                PMPI_Type_size(origin_datatype, &data_size);
-                data_size *= origin_count;
-            }
-#endif
-            mpi_errno = MTCORE_Get_helper_rank(target_rank, 0, 1, data_size, uh_win,
-                                               &target_h_rank_in_uh, &target_h_offset);
-            if (mpi_errno != MPI_SUCCESS)
-                goto fn_fail;
-
-            uh_target_disp = target_h_offset + uh_win->targets[target_rank].disp_unit * target_disp;
-
-            /* Issue operation to the helper process in corresponding uh-window of target process. */
-            mpi_errno = PMPI_Accumulate(origin_addr, origin_count, origin_datatype,
-                                        target_h_rank_in_uh, uh_target_disp,
-                                        target_count, target_datatype, op, *win_ptr);
-
-            MTCORE_DBG_PRINT("MTCORE Accumulate to (helper %d, win 0x%x [%s]) instead of "
-                             "target %d, 0x%lx(0x%lx + %d * %ld)\n",
-                             target_h_rank_in_uh, *win_ptr,
-                             MTCORE_Win_epoch_stat_name[uh_win->epoch_stat],
-                             target_rank, uh_target_disp, target_h_offset,
-                             uh_win->targets[target_rank].disp_unit, target_disp);
+        if (MTCORE_ENV.load_opt == MTCORE_LOAD_BYTE_COUNTING) {
+            PMPI_Type_size(origin_datatype, &data_size);
+            data_size *= origin_count;
         }
+#endif
+        mpi_errno = MTCORE_Get_helper_rank(target_rank, 0, 1, data_size, uh_win,
+                                           &target_h_rank_in_uh, &target_h_offset);
+        if (mpi_errno != MPI_SUCCESS)
+            goto fn_fail;
+
+        uh_target_disp = target_h_offset + uh_win->targets[target_rank].disp_unit * target_disp;
+
+        /* Issue operation to the helper process in corresponding uh-window of target process. */
+        mpi_errno = PMPI_Accumulate(origin_addr, origin_count, origin_datatype,
+                                    target_h_rank_in_uh, uh_target_disp,
+                                    target_count, target_datatype, op, *win_ptr);
+
+        MTCORE_DBG_PRINT("MTCORE Accumulate to (helper %d, win 0x%x [%s]) instead of "
+                         "target %d, 0x%lx(0x%lx + %d * %ld)\n",
+                         target_h_rank_in_uh, *win_ptr,
+                         MTCORE_Win_epoch_stat_name[uh_win->epoch_stat],
+                         target_rank, uh_target_disp, target_h_offset,
+                         uh_win->targets[target_rank].disp_unit, target_disp);
     }
 
   fn_exit:
--- src/mpi/rma/fetch_and_op.c
+++ src/mpi/rma/fetch_and_op.c
@@ -2,34 +2,6 @@
 #include <stdlib.h>
 #include "mtcore.h"
 
-static int MTCORE_Fetch_and_op_shared_impl(const void *origin_addr, void *result_addr,
-                                           MPI_Datatype datatype, int target_rank,
-                                           MPI_Aint target_disp, MPI_Op op, MPI_Win win,
-                                           MTCORE_Win * uh_win)
-{
-    int mpi_errno = MPI_SUCCESS;
-    MPI_Win *win_ptr = &uh_win->my_uh_win;
-
-    MTCORE_Get_epoch_local_win(uh_win, win_ptr);
-
-    /* Issue operation to the target through local window, because shared
-     * communication is fully handled by local process.
-     */
-    mpi_errno = PMPI_Fetch_and_op(origin_addr, result_addr, datatype,
-                                  uh_win->my_rank_in_uh_comm, target_disp, op, *win_ptr);
-    MTCORE_DBG_PRINT("MTCORE Fetch_and_op to self(%d, in local win 0x%x)\n",
-                     uh_win->my_rank_in_uh_comm, *win_ptr);
-
-    goto fn_exit;
-
-  fn_exit:
-    return mpi_errno;
-
-  fn_fail:
-    goto fn_exit;
-}
-
-
 static int MTCORE_Fetch_and_op_segment_impl(const void *origin_addr, void *result_addr,
                                             MPI_Datatype datatype, int target_rank,
                                             MPI_Aint target_disp, MPI_Op op, MPI_Win win,
@@ -103,76 +75,62 @@ static int MTCORE_Fetch_and_op_impl(const void *origin_addr, void *result_addr,
     int rank;
 
     PMPI_Comm_rank(uh_win->user_comm, &rank);
-#ifdef MTCORE_ENABLE_LOCAL_LOCK_OPT
-    if (target_rank == rank && uh_win->is_self_locked) {
-        /* If target is itself, we do not need translate it to any Helpers because
-         * win_lock(self) will force lock(helper) to be granted so that it is safe
-         * to send operations to the real target.
-         */
-        mpi_errno = MTCORE_Fetch_and_op_shared_impl(origin_addr, result_addr,
-                                                    datatype, target_rank, target_disp, op, win,
-                                                    uh_win);
+
+    /* Should not do local RMA in accumulate because of atomicity issue */
+
+    /* TODO: Do we need segment load balancing in fence ?
+     * 1. No lock issue.
+     * 2. overhead of data range checking and division */
+
+    /* Although only one predefined datatype element is transferred in such
+     * operation, we still need call segmentation routine to get its the segment
+     * number if target is divided to multiple segments. */
+    if (MTCORE_ENV.lock_binding == MTCORE_LOCK_BINDING_SEGMENT &&
+        uh_win->targets[target_rank].num_segs > 1 && uh_win->epoch_stat == MTCORE_WIN_EPOCH_LOCK) {
+        mpi_errno = MTCORE_Fetch_and_op_segment_impl(origin_addr, result_addr,
+                                                     datatype, target_rank, target_disp, op,
+                                                     win, uh_win);
         if (mpi_errno != MPI_SUCCESS)
             return mpi_errno;
     }
-    else
-#endif
-    {
-        /* TODO: Do we need segment load balancing in fence ?
-         * 1. No lock issue.
-         * 2. overhead of data range checking and division */
+    else {
+        /* Translation for intra/inter-node operations.
+         *
+         * We do not use force flush + shared window for optimizing operations to local targets.
+         * Because: 1) we lose lock optimization on force flush; 2) Although most implementation
+         * does shared-communication for operations on shared windows, MPI standard doesn’t
+         * require it. Some implementation may use network even for shared targets for
+         * shorter CPU occupancy.
+         */
+        int target_h_rank_in_uh = -1;
+        int data_size = 0;
+        MPI_Aint target_h_offset = 0;
+        MPI_Win *win_ptr = NULL;
 
-        /* Although only one predefined datatype element is transferred in such
-         * operation, we still need call segmentation routine to get its the segment
-         * number if target is divided to multiple segments. */
-        if (MTCORE_ENV.lock_binding == MTCORE_LOCK_BINDING_SEGMENT &&
-            uh_win->targets[target_rank].num_segs > 1 &&
-            uh_win->epoch_stat == MTCORE_WIN_EPOCH_LOCK) {
-            mpi_errno = MTCORE_Fetch_and_op_segment_impl(origin_addr, result_addr,
-                                                         datatype, target_rank, target_disp, op,
-                                                         win, uh_win);
-            if (mpi_errno != MPI_SUCCESS)
-                return mpi_errno;
-        }
-        else {
-            /* Translation for intra/inter-node operations.
-             *
-             * We do not use force flush + shared window for optimizing operations to local targets.
-             * Because: 1) we lose lock optimization on force flush; 2) Although most implementation
-             * does shared-communication for operations on shared windows, MPI standard doesn’t
-             * require it. Some implementation may use network even for shared targets for
-             * shorter CPU occupancy.
-             */
-            int target_h_rank_in_uh = -1;
-            int data_size = 0;
-            MPI_Aint target_h_offset = 0;
-            MPI_Win *win_ptr = NULL;
-
-            MTCORE_Get_epoch_win(target_rank, 0, uh_win, win_ptr);
+        MTCORE_Get_epoch_win(target_rank, 0, uh_win, win_ptr);
 
 #if defined(MTCORE_ENABLE_RUNTIME_LOAD_OPT)
-            if (MTCORE_ENV.load_opt == MTCORE_LOAD_BYTE_COUNTING) {
-                PMPI_Type_size(datatype, &data_size);
-            }
-#endif
-            mpi_errno = MTCORE_Get_helper_rank(target_rank, 0, 1, data_size, uh_win,
-                                               &target_h_rank_in_uh, &target_h_offset);
-            if (mpi_errno != MPI_SUCCESS)
-                goto fn_fail;
-
-            uh_target_disp = target_h_offset + uh_win->targets[target_rank].disp_unit * target_disp;
-
-            /* Issue operation to the helper process in corresponding uh-window of target process. */
-            mpi_errno = PMPI_Fetch_and_op(origin_addr, result_addr, datatype, target_h_rank_in_uh,
-                                          uh_target_disp, op, *win_ptr);
-
-            MTCORE_DBG_PRINT("MTCORE Fetch_and_op to (helper %d, win 0x%x [%s]) instead of "
-                             "target %d, 0x%lx(0x%lx + %d * %ld)\n",
-                             target_h_rank_in_uh, *win_ptr,
-                             MTCORE_Win_epoch_stat_name[uh_win->epoch_stat],
-                             target_rank, uh_target_disp, target_h_offset,
-                             uh_win->targets[target_rank].disp_unit, target_disp);
+        if (MTCORE_ENV.load_opt == MTCORE_LOAD_BYTE_COUNTING) {
+            PMPI_Type_size(datatype, &data_size);
         }
+#endif
+        mpi_errno = MTCORE_Get_helper_rank(target_rank, 0, 1, data_size, uh_win,
+                                           &target_h_rank_in_uh, &target_h_offset);
+        if (mpi_errno != MPI_SUCCESS)
+            goto fn_fail;
+
+        uh_target_disp = target_h_offset + uh_win->targets[target_rank].disp_unit * target_disp;
+
+        /* Issue operation to the helper process in corresponding uh-window of target process. */
+        mpi_errno = PMPI_Fetch_and_op(origin_addr, result_addr, datatype, target_h_rank_in_uh,
+                                      uh_target_disp, op, *win_ptr);
+
+        MTCORE_DBG_PRINT("MTCORE Fetch_and_op to (helper %d, win 0x%x [%s]) instead of "
+                         "target %d, 0x%lx(0x%lx + %d * %ld)\n",
+                         target_h_rank_in_uh, *win_ptr,
+                         MTCORE_Win_epoch_stat_name[uh_win->epoch_stat],
+                         target_rank, uh_target_disp, target_h_offset,
+                         uh_win->targets[target_rank].disp_unit, target_disp);
     }
 
   fn_exit:
