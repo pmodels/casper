539714846bef60edd0a38426a77d12727332c32e
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2015-06-17 11:27:44 -0500

    Changed autogen.sh to support user specified mpi path.
    autogen.sh checks mpi.h in three steps:
    (1) Get a default path by checking `which mpicc` (may be undefined)
    (2) If user specifies <path>, check <path>/include/mpi.h. If found, use
    it to generate wrapper functions, otherwise exit error.
    (3) If user does not specify, check default path. If found, use it
    otherwise exit error.

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,38 @@
 #! /bin/bash
 
-path=`which mpicc | sed -e 's_/[^/]*$__g'`/../include
-path=`cd $path ; pwd ; cd $OLDPWD`/mpi.h
+# default MPI path
+mpidir=`which mpicc| sed -e 's_/[^/]*$__g'`/../
+mpih_path=
+
+# user specified MPI path
+for arg in "$@" ; do
+    case $arg in
+        -with-mpi=*|--with-mpi=*)
+            mpidir=`echo "A$arg" | sed -e 's/.*=//'`
+        ;;
+
+        --help|-h|-help)
+        echo "./autogen.sh --with-mpi=<mpi installation path>"
+        exit
+        ;;
+
+    esac
+done
+
+echo -n "Checking header file mpi.h at $mpidir ..."
+_mpih=$mpidir/include/mpi.h
+if [ ! -f $_mpih ];then
+    echo "not found (error)"
+    exit 1
+else
+    echo "done"
+    mpih_path=`cd $(dirname $_mpih) ; pwd ; cd $OLDPWD`/mpi.h
+fi
+
+echo "Found $mpih_path"
 
 echo -n "Generating MPI wrappers... "
-./src/buildiface --infile $path --outfile src/mpi_wrap.c
+./src/buildiface --infile $mpih_path --outfile src/mpi_wrap.c
 echo "done"
 
 autoreconf -vif
