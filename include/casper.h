7c1a5ce2ab06a35aa1a1d95edd485666699fcb43
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2015-07-17 23:48:11 -0500

    Defined a query function to check the size of ghost processes.

---
+++ include/casper.h
@@ -0,0 +1,12 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ * (C) 2015 by Argonne National Laboratory.
+ *     See COPYRIGHT in top-level directory.
+ */
+#ifndef CASPER_H_
+#define CASPER_H_
+
+/* Get the number of ghost processes. */
+int CSP_ghost_size(int *ng);
+
+#endif /* CASPER_H_ */
---
+++ src/casper.c
@@ -0,0 +1,23 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ * (C) 2015 by Argonne National Laboratory.
+ *     See COPYRIGHT in top-level directory.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <casper.h>
+#include "csp.h"
+
+
+/* This file defines Casper external function. */
+
+/* Get the number of ghost processes. */
+int CSP_ghost_size(int *ng)
+{
+    if (ng == NULL)
+        return -1;
+
+    (*ng) = CSP_ENV.num_g;
+    return 0;
+}
