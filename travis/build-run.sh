019c2abc7dec41e5778155c81fadd11304721f7a
Jeff Hammond <jeff.science@gmail.com>
2016-09-15 15:44:45 -0700

    run real tests

--- travis/build-run.sh
+++ travis/build-run.sh
@@ -32,9 +32,16 @@ esac
 
 # Configure and build
 ./autogen.sh
-./configure CC=mpicc CFLAGS="-std=c99" --disable-static
+./configure CC=mpicc CFLAGS="-std=c99" --disable-static --prefix=/tmp
+make V=1
+make V=1 install
+
+export CSP_VERBOSE=1
 
 # Run unit tests
-export CSP_VERBOSE=1
-make V=1
 make V=1 check
+
+# Run real tests
+cd test
+make V=1
+make V=1 MPIEXEC="mpirun -host localhost " testing
