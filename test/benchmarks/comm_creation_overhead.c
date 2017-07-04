bee50700e1a7390717d6e993168d33d3b2fe6c59
Min Si <msi@anl.gov>
2017-07-03 22:17:41 -0500

    Test modifies.

--- test/benchmarks/comm_creation_overhead.c
+++ test/benchmarks/comm_creation_overhead.c
@@ -7,6 +7,8 @@
 
 static int iters = DEFAULT_ITERS;
 static MPI_Comm *comm;
+static int comm_rank, comm_size;
+static char testname[128] = { 0 };
 
 static void usage(void)
 {
@@ -15,11 +17,97 @@ static void usage(void)
     exit(1);
 }
 
+static void set_testname(void)
+{
+    char *val = getenv("TEST_NAME");
+    if (val && strlen(val) > 0) {
+        strncpy(testname, val, 128);
+    }
+}
+
+static void run_split(void)
+{
+    double start, end, local_time, avg_time;
+    int i;
+
+    start = MPI_Wtime();
+    for (i = 0; i < iters; i++)
+        MPI_Comm_split(MPI_COMM_WORLD, 0, 0, &comm[i]);
+    end = MPI_Wtime();
+
+    local_time = end - start;
+    MPI_Reduce(&local_time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    avg_time /= comm_size;
+
+    if (comm_rank == 0)
+        printf("%s comm_split: %.3f us\n", testname, 1e6 * (end - start) / iters);
+}
+
+static void run_dup(void)
+{
+    double start, end, local_time, avg_time;
+    int i;
+
+    start = MPI_Wtime();
+    for (i = 0; i < iters; i++)
+        MPI_Comm_dup(MPI_COMM_WORLD, &comm[i]);
+    end = MPI_Wtime();
+
+    local_time = end - start;
+    MPI_Reduce(&local_time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    avg_time /= comm_size;
+
+    if (comm_rank == 0)
+        printf("%s comm_dup: %.3f us\n", testname, 1e6 * (end - start) / iters);
+}
+
+static void run_create(void)
+{
+    double start, end, local_time, avg_time;
+    int i;
+    MPI_Group group;
+
+    MPI_Comm_group(MPI_COMM_WORLD, &group);
+
+    start = MPI_Wtime();
+    for (i = 0; i < iters; i++)
+        MPI_Comm_create(MPI_COMM_WORLD, group, &comm[i]);
+    end = MPI_Wtime();
+
+    MPI_Group_free(&group);
+
+    local_time = end - start;
+    MPI_Reduce(&local_time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    avg_time /= comm_size;
+
+    if (comm_rank == 0)
+        printf("%s comm_create: %.3f us\n", testname, 1e6 * (end - start) / iters);
+}
+
+static void run_free(const char *creat_type)
+{
+    double start, end, local_time, avg_time;
+    int i;
+
+    /* free time */
+    start = MPI_Wtime();
+    for (i = 0; i < iters; i++)
+        MPI_Comm_free(&comm[i]);
+    end = MPI_Wtime();
+
+    local_time = end - start;
+    MPI_Reduce(&local_time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    avg_time /= comm_size;
+
+    if (comm_rank == 0)
+        printf("%s %s_free %.3f us\n", testname, creat_type, 1e6 * (end - start) / iters);
+}
+
 int main(int argc, char **argv)
 {
     int i;
     double start, end, local_time, avg_time;
-    int comm_rank, comm_size;
+    MPI_Info info = MPI_INFO_NULL;
 
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
@@ -36,69 +124,30 @@ int main(int argc, char **argv)
         }
     }
 
+    set_testname();
+
+#if defined(USE_DUPCOMM)
+    MPI_Info_create(&info);
+    MPI_Info_set(info, (char *) "wildcard_used", (char *) "anysrc|anytag_notag");
+    MPI_Comm_set_info(MPI_COMM_WORLD, info);
+    MPI_Info_free(&info);
+#elif defined(USE_TAGTRANS)
+    MPI_Info_create(&info);
+    MPI_Info_set(info, (char *) "wildcard_used", (char *) "none");
+    MPI_Comm_set_info(MPI_COMM_WORLD, info);
+    MPI_Info_free(&info);
+#endif
+
     comm = (MPI_Comm *) malloc(iters * sizeof(MPI_Comm));
 
+    run_split();
+    run_free("comm_split");
 
-    /* split time */
-    start = MPI_Wtime();
-    for (i = 0; i < iters; i++)
-        MPI_Comm_split(MPI_COMM_WORLD, 0, 0, &comm[i]);
-    end = MPI_Wtime();
+    run_create();
+    run_free("comm_create");
 
-    local_time = end - start;
-    MPI_Reduce(&local_time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    avg_time /= comm_size;
-
-    if (comm_rank == 0)
-        printf("comm_split: %.3f us\n", 1e6 * (end - start) / iters);
-
-
-    /* free time */
-    start = MPI_Wtime();
-    for (i = 0; i < iters; i++)
-        MPI_Comm_free(&comm[i]);
-    end = MPI_Wtime();
-
-    local_time = end - start;
-    MPI_Reduce(&local_time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    avg_time /= comm_size;
-
-    if (comm_rank == 0)
-        printf("comm_free for split comm: %.3f us\n", 1e6 * (end - start) / iters);
-
-
-    /* create time */
-    MPI_Group group;
-    MPI_Comm_group(MPI_COMM_WORLD, &group);
-
-    start = MPI_Wtime();
-    for (i = 0; i < iters; i++)
-        MPI_Comm_create(MPI_COMM_WORLD, group, &comm[i]);
-    end = MPI_Wtime();
-
-    MPI_Group_free(&group);
-
-    local_time = end - start;
-    MPI_Reduce(&local_time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    avg_time /= comm_size;
-
-    if (comm_rank == 0)
-        printf("comm_create: %.3f us\n", 1e6 * (end - start) / iters);
-
-
-    /* free time */
-    start = MPI_Wtime();
-    for (i = 0; i < iters; i++)
-        MPI_Comm_free(&comm[i]);
-    end = MPI_Wtime();
-
-    local_time = end - start;
-    MPI_Reduce(&local_time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    avg_time /= comm_size;
-
-    if (comm_rank == 0)
-        printf("comm_free for created comm: %.3f us\n", 1e6 * (end - start) / iters);
-
+    run_dup();
+    run_free("comm_dup");
 
     free(comm);
 
--- test/benchmarks/halo.c
+++ test/benchmarks/halo.c
@@ -9,6 +9,7 @@
 
 static int iters = DEFAULT_ITERS;
 static int dim = DEFAULT_DIM;
+static char testname[128] = { 0 };
 
 static void usage(void)
 {
@@ -17,7 +18,15 @@ static void usage(void)
     exit(1);
 }
 
-#define ind(x,y)  (x * (dim + 2) + y)
+static void set_testname(void)
+{
+    char *val = getenv("TEST_NAME");
+    if (val && strlen(val) > 0) {
+        strncpy(testname, val, 128);
+    }
+}
+
+#define ind(x,y)  ((x) * (dim + 2) + (y))
 
 int main(int argc, char **argv)
 {
@@ -35,6 +44,8 @@ int main(int argc, char **argv)
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+
+    set_testname();
 
     while (--argc && ++argv) {
         if (!strcmp(*argv, "--iters")) {
@@ -59,11 +70,22 @@ int main(int argc, char **argv)
     MPI_Type_commit(&type);
 
     MPI_Dims_create(comm_size, 2, dims);
+
+    MPI_Info info = MPI_INFO_NULL;
+    MPI_Info_create(&info);
+    MPI_Info_set(info, (char *) "wildcard_used", (char *) "none");
+    MPI_Comm_set_info(MPI_COMM_WORLD, info);
+    MPI_Info_free(&info);
+
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 1, &comm);
     assert(comm != MPI_COMM_NULL);
 
     MPI_Cart_shift(comm, 0, 1, &north, &south);
     MPI_Cart_shift(comm, 1, 1, &west, &east);
+
+#ifdef STEP_TIME
+    double cp_t0 = 0, cp_time = 0, avg_cp_time = 0;
+#endif
 
     start = MPI_Wtime();
     for (i = 0; i < iters; i++) {
@@ -72,7 +94,10 @@ int main(int argc, char **argv)
         MPI_Irecv(&outbuf[ind(1, 0)], 1, type, west, 0, comm, &req[2]);
         MPI_Irecv(&outbuf[ind(1, dim + 1)], 1, type, east, 0, comm, &req[3]);
 
-        for (j = j; j <= dim; j++) {
+#ifdef STEP_TIME
+        cp_t0 = MPI_Wtime();
+#endif
+        for (j = 1; j <= dim; j++) {
             for (k = 1; k <= dim; k++) {
                 outbuf[ind(j, k)] =
                     (inbuf[ind(j, k - 1)] + inbuf[ind(j, k + 1)] + inbuf[ind(j - 1, k)] +
@@ -84,6 +109,9 @@ int main(int argc, char **argv)
         MPI_Isend(&outbuf[ind(dim, 1)], dim, MPI_DOUBLE, south, 0, comm, &req[5]);
         MPI_Isend(&outbuf[ind(1, 1)], 1, type, west, 0, comm, &req[6]);
         MPI_Isend(&outbuf[ind(1, dim)], 1, type, east, 0, comm, &req[7]);
+#ifdef STEP_TIME
+        cp_time += MPI_Wtime() - cp_t0;
+#endif
 
         MPI_Waitall(8, req, MPI_STATUSES_IGNORE);
 
@@ -98,8 +126,17 @@ int main(int argc, char **argv)
     MPI_Reduce(&local_time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     avg_time /= comm_size;
 
-    if (comm_rank == 0)
-        printf("average time: %.3f us\n", 1e6 * (end - start) / iters);
+#ifdef STEP_TIME
+    MPI_Reduce(&cp_time, &avg_cp_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    avg_cp_time /= comm_size;
+#endif
+
+    if (comm_rank == 0) {
+        printf("%s average_time: %.3f us\n", testname, 1e6 * avg_time / iters);
+#ifdef STEP_TIME
+        printf("%s compute_time: %.3f us\n", testname, 1e6 * avg_cp_time / iters);
+#endif
+    }
 
     free(inbuf);
     free(outbuf);
