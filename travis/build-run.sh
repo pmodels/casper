2f905281f478944e05c5236c3168e2e73e780d69
Hammond, Jeff R <jeff.r.hammond@intel.com>
2019-11-16 12:10:08 -0800

    update MPI versions and fix Travis CI issues
    install autotools on macos using brew
    update to latest MPI release downloads
    add autogen.sh since mpich github releases no ship with configure
    stupid useless github tarballs
    accept automake >= 1.15
    silence excessive output from Open-MPI mpi.h

--- travis/build-run.sh
+++ travis/build-run.sh
@@ -29,12 +29,14 @@ case "$MPI_IMPL" in
         mpicc --showme:command
         # see https://github.com/open-mpi/ompi/issues/2956
         export TMPDIR=/tmp
+        # suppress excessive output
+        OMPI_NO_WARN="-Wno-unknown-attributes"
         ;;
 esac
 
 # Configure and build
 ./autogen.sh
-./configure CC=mpicc CFLAGS="-std=c99" --disable-static --prefix=/tmp
+./configure CC=mpicc CFLAGS="-std=c99 $OMPI_NO_WARN" --disable-static --prefix=/tmp
 make V=1
 make V=1 install
 
